5f923b00fea315ff82c045e9eebab40cee9e9c71
Decimus <decimus.titius@gmail.com>
2016-01-25 20:54:12 -0600

    Adding reflection tests to check IApplicationUser interface is properly constructed.

--- ApplicationUser/UnitTestUserLibrary/UserUnitTests.cs
+++ ApplicationUser/UnitTestUserLibrary/UserUnitTests.cs
@@ -1,14 +1,60 @@
 ﻿namespace DecimusN7.Sandbox.ApplicationUser.Tests
 {
+    using System.Linq;
+    using System.Reflection;
     using ApplicationUser;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
-    using System.Reflection;
-    using System.Linq;
 
     [TestClass]
     public class UserUnitTests
     {
         private Assembly applicationUserAssembly = Assembly.Load("DecimusN7.Sandbox.ApplicationUser");
+
+        private PropertyInfo[] Properties
+        {
+            get
+            {
+                return this.applicationUserAssembly.GetTypes()
+                    .Where(x => x.Name == "IApplicationUser")
+                    .SingleOrDefault().GetProperties();
+            }
+        }
+
+        [TestMethod]
+        public void ApplicationUserInterfaceExists()
+        {
+            Assert.IsTrue(this.applicationUserAssembly.GetTypes()
+                .Any(x => x.FullName == "DecimusN7.Sandbox.ApplicationUser.IApplicationUser"));
+        }
+
+        [TestMethod]
+        public void ApplicationUserInterfacePropertiesExist()
+        {
+            Assert.IsTrue(this.Properties
+                .Any(x => x.Name == "Id"));
+
+            Assert.IsTrue(this.Properties
+                .Any(x => x.Name == "FirstName"));
+
+            Assert.IsTrue(this.Properties
+                .Any(x => x.Name == "LastName"));
+        }
+
+        [TestMethod]
+        public void ApplicationUserInterfacePropertiesHaveCorrectType()
+        {
+            Assert.IsTrue(this.Properties
+                .Where(x => x.Name == "Id")
+                .Single().PropertyType == typeof(int));
+
+            Assert.IsTrue(this.Properties
+                .Where(x => x.Name == "FirstName")
+                .Single().PropertyType == typeof(string));
+
+            Assert.IsTrue(this.Properties
+                .Where(x => x.Name == "LastName")
+                .Single().PropertyType == typeof(string));
+        }
 
         [TestMethod]
         public void UserPropertiesExist()
@@ -21,12 +67,6 @@
             Assert.AreEqual(1, user.Id);
             Assert.AreEqual("Simpson", user.LastName);
             Assert.AreEqual("Homer", user.FirstName);
-        }
-
-        [TestMethod]
-        public void ApplicationUserInterfaceExists()
-        {
-            Assert.IsTrue(applicationUserAssembly.GetTypes().Contains(typeof(IApplicationUser)));
         }
     }
 }
